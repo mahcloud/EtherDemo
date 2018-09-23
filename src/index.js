5c71bc75131d893ab371b07bc75e8a529f9e4c41
Mike Hoffert <mike.hoffert@gmail.com>
2018-09-23 13:29:29 -0600

    Demo app assets

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import { Ellipsis } from "@alchemyalcove/ether";
+import { render } from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <Ellipsis>Demo Coming Soon</Ellipsis>,
+    document.getElementById("demo")
+  );
+});
